c7f3998286ecefffc09e0a1c77310bf0ccf40311
AJ Ruf <z0isch@gmail.com>
2023-08-24 10:49:22 -0400

    Remove kdt

--- src/Birdson.hs
+++ src/Birdson.hs
@@ -4,9 +4,7 @@ module Birdson (runBirdson) where
 
 import Control.Lens (use, (%=))
 import Control.Lens.TH
-import Data.KdTree.Dynamic (KdTree)
-import Data.KdTree.Dynamic qualified as KDT
-import GHC.Float (int2Double)
+import Data.Semigroup (Min (..))
 import Import
 import Linear
 import RIO.List qualified as L
@@ -17,12 +15,11 @@ import RIO.State
     StateT,
     evalStateT,
   )
-import System.Random (randomRIO)
+import System.Random (Random, randomRIO)
 
 data Birdson = Birdson
   { _birdsonActive :: Set (V2 Int),
-    _birdsonInactive :: [V2 Int],
-    _birdsonKDT :: KdTree Int (V2 Int),
+    _birdsonInactive :: Set (V2 Int),
     _birdsonRadius :: Int,
     _birdsonCandidates :: Int,
     _birdsonBounds :: Int
@@ -32,7 +29,7 @@ data Birdson = Birdson
 $(makeLenses ''Birdson)
 
 runBirdson :: MonadIO m => Int -> m [V2 Int]
-runBirdson = evalStateT birdson <=< initialBirdson
+runBirdson = fmap toList . evalStateT birdson <=< initialBirdson
 
 initialBirdson :: MonadIO m => Int -> m Birdson
 initialBirdson bounds = do
@@ -44,46 +41,56 @@ initialBirdson bounds = do
     Birdson
       { _birdsonActive = Set.singleton initial,
         _birdsonInactive = mempty,
-        _birdsonKDT = KDT.singleton (\(V2 a b) -> [a, b]) initial,
         _birdsonRadius = round $ fromIntegral @_ @Double bounds * 0.2,
         _birdsonCandidates = 25,
         _birdsonBounds = bounds
       }
 
-birdson :: MonadIO m => StateT Birdson m [V2 Int]
+birdson :: MonadIO m => StateT Birdson m (Set (V2 Int))
 birdson = go >> use birdsonInactive
   where
     go = do
       b <- get
       active <- randomEl $ b ^. birdsonActive
-      candidates <- randomInAnnulus active b
+      candidates <- randomInBirdsonAnnulus active b
       let validPoint p =
             inBounds (b ^. birdsonBounds) p
-              && distanceToNearestNeighbor (b ^. birdsonKDT) p >= int2Double (b ^. birdsonRadius)
+              && quadranceToNearestNeighbor b p >= (b ^. birdsonRadius * b ^. birdsonRadius)
       case L.dropWhile (not . validPoint) candidates of
         (c : _) -> do
-          birdsonKDT %= (`KDT.insert` c)
           birdsonActive %= Set.insert c
         [] -> do
           birdsonActive %= Set.delete active
-          birdsonInactive %= (active :)
+          birdsonInactive %= Set.insert active
       unlessM (Set.null <$> use birdsonActive) go
 
 randomEl :: MonadIO f => Set a -> f a
 randomEl s = (`Set.elemAt` s) <$> randomRIO (0, Set.size s - 1)
 
 inBounds :: Int -> V2 Int -> Bool
-inBounds r (V2 x y) = (x * x + y * y) <= r * r
+inBounds r v = quadrance v <= r * r
 
-randomInAnnulus :: MonadIO m => V2 Int -> Birdson -> m [V2 Int]
-randomInAnnulus around Birdson {..} =
-  for
-    [0 .. _birdsonCandidates]
-    ( \_ -> do
-        d <- randomRIO (_birdsonRadius, _birdsonRadius * 2)
-        a <- fmap round . angle <$> randomRIO (0, 2 * pi @Double)
-        pure $ around + (d *^ a)
-    )
+randomInBirdsonAnnulus :: MonadIO m => V2 Int -> Birdson -> m [V2 Int]
+randomInBirdsonAnnulus around Birdson {..} = for [0 .. _birdsonCandidates] $ \_ ->
+  fmap round
+    <$> randomInAnnulus
+      (toDouble <$> around)
+      ( toDouble _birdsonRadius,
+        toDouble $ _birdsonRadius * 2
+      )
+  where
+    toDouble = fromIntegral @_ @Double
 
-distanceToNearestNeighbor :: KdTree Int (V2 Int) -> V2 Int -> Double
-distanceToNearestNeighbor kdt v = distance (int2Double <$> KDT.nearest kdt v) (int2Double <$> v)
+type Annulus a = (a, a)
+
+randomInAnnulus :: (Random a, MonadIO m, Floating a) => V2 a -> Annulus a -> m (V2 a)
+randomInAnnulus around (r1, r2) = do
+  d <- randomRIO (r1, r2)
+  a <- angle <$> randomRIO (0, 2 * pi)
+  pure $ around + (d *^ a)
+
+quadranceToNearestNeighbor :: Birdson -> V2 Int -> Int
+quadranceToNearestNeighbor b p =
+  getMin $
+    foldMap (Min . qd p) $
+      b ^. birdsonActive <> b ^. birdsonInactive
